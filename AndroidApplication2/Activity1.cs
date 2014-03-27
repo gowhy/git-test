99bc6e06cbe3a81017eab2c2d85436d635ae835e
286379175 <376007434@qq.com>
2014-03-27 19:24:56 +0800

    调试代码我测试下哈哈哈

--- AndroidApplication2/Activity1.cs
+++ AndroidApplication2/Activity1.cs
@@ -17,9 +17,13 @@ namespace AndroidApplication2
         protected override void OnCreate(Bundle bundle)
         { 
  
+              
 
 
-            base.OnCreate(bundle);
+
+
+
+            base.OnCreate(bundle);      
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
